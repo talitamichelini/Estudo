4868b2e58148f74e5836ee9fcf1b79d2b5df1c57
Talita Michelini <talita.michelini@yahoo.com.br>
2025-11-10 19:18:59 -0300

    Projetos em variaveis

---
+++ variaveis.py
@@ -0,0 +1,34 @@
+nome = "Talita" #string
+idade = 29 #inteiro
+altura = 1.75 #float
+cidade = "São Paulo" #string
+dev = True #booleano (True ou False)
+
+#atalho para comentar geral = ctrl + k + c
+#atalho para descomentar geral = ctrl + k + u
+
+
+
+#<nome da var> = <valor>
+
+#o valor deve ser dado antes do uso da variável, do código.
+
+
+
+print(f"Olá, {nome}! Você tem {idade} anos, mede {altura} metros de altura, mora em {cidade} e é desenvolvedora {dev}.")
+
+#f = format = formatação
+
+
+# nome = input("Digite seu nome: ") #input sempre retorna string
+# idade = input("Digite sua idade: ") #input = função para receber dados do usuário
+# altura = input("Digite sua altura: ")
+# cidade = input("Digite sua cidade: ")
+# dev = input("Você é desenvolvedor? (sim/não): ")
+
+# print(f"Olá, {nome}! Você tem {idade} anos, mede {altura} metros de altura, mora em {cidade} e é {dev}.")
+
+#LEMBRAR DE SALVAR ANTES DE RODAR O CÓDIGO!
+
+
+
