6690dc9c225c2b68ded1d6a6809c2ca809c2225a
Talita Michelini <talita.michelini@yahoo.com.br>
2025-11-16 16:19:47 -0300

    Adivinhe o número secreto

---
+++ projeto1.py
@@ -0,0 +1,23 @@
+# Jogo de Adivinhação
+
+# No jogo, o usuário precisa adivinhar o número secreto
+# Pode tentar várias vezes até acertar
+
+# snake_case = underline (usado em Python)
+# camelCase = letraMaiusculaNoMeio (usado em Go)
+
+numero_secreto = 5
+tentativa = 0
+
+while tentativa != numero_secreto:
+    # != diferente
+    tentativa = int(input("Digite um número: "))
+
+# para dar dicas ao usuário
+    if tentativa < numero_secreto:
+        print("Tente um número maior.")
+    elif tentativa > numero_secreto:
+        print("Tente um número menor.")
+    else:
+        print("Parabéns! Você acertou o número secreto.")
+# Fim do jogo
