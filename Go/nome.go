a4ca3b437a0d031b6978c440cd11d7212f69707e
Talita Michelini <talita.michelini@yahoo.com.br>
2025-11-20 23:49:49 -0300

    Move arquivos para nova estrutura de pastas

---
+++ Go/nome.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nome string
+	fmt.Print("Digite seu nome: ")
+
+	nome = "Talita" // Simulando a entrada do usuário
+	fmt.Printf("Muito prazer,", nome)
+}
+
+
+ 
