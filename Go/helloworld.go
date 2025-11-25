fb9f36b4cc96ebe6e8e5d1b150342ecd4a0a905e
Talita Michelini <talita.michelini@yahoo.com.br>
2025-11-24 23:03:00 -0300

    Hello World em Go anotações

--- Go/helloworld.go
+++ Go/helloworld.go
@@ -1,9 +1,19 @@
 package main
 
+// package main = ponto de entrada do programa
+
 import "fmt"
 
+// fmt significa “format” e é o pacote de formatação de entrada e saída do Go.
+// Ele fornece funções para: imprimir texto, ler dados do usuário
 func main() {
 	fmt.Println("Hello, World!")
 
 }
 
+// func = Onde o programa começa a ser executado. Tudo começa por aqui
+// dentro da função principal - Print "ln" = pula uma linha
+
+// no site do go tem diversos exemplos de códigos e pacotes
+
+// lembrar de ver se o arquivo está na pasta certa para rodar
