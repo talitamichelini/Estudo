00a639cfe783e962f6c1ba2e39ee2b3130950ce1
Talita Michelini <talita.michelini@yahoo.com.br>
2025-11-25 14:22:05 -0300

    adiciona anotações de estudo

--- Go/helloworld.go
+++ Go/helloworld.go
@@ -1,9 +1,10 @@
 package main
 
 // package main = ponto de entrada do programa
-
+// pacote = conjunto de funções
 import "fmt"
 
+// nesse caso o pacote é fmt = algum lugar do código tem fmt
 // fmt significa “format” e é o pacote de formatação de entrada e saída do Go.
 // Ele fornece funções para: imprimir texto, ler dados do usuário
 func main() {
@@ -11,9 +12,11 @@ func main() {
 
 }
 
+// nome do pacote.nome da função desse pacote
+
 // func = Onde o programa começa a ser executado. Tudo começa por aqui
 // dentro da função principal - Print "ln" = pula uma linha
 
 // no site do go tem diversos exemplos de códigos e pacotes
 
-// lembrar de ver se o arquivo está na pasta certa para rodar
+// *lembrar de ver se o arquivo está na pasta certa para rodar
